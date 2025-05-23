dfe0e54e195f5d1a06a8d5c1db76956437eed811
dannwold <wold.dan@gmail.com>
2025-05-23 02:20:20 -0500

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,2 +1,4 @@
 @echo off
-REM Gradle wrapper script
+set DIR=%~dp0
+set WRAPPER_JAR=%DIR%gradle\wrapper\gradle-wrapper.jar
+"%JAVA_HOME%\bin\java" -cp "%WRAPPER_JAR%" org.gradle.wrapper.GradleWrapperMain %*
